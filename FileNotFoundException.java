3e3b02afbd7ab62a6b17448755ae074eafb9c308
korneevra <45228010+korneevra@users.noreply.github.com>
2023-04-21 20:34:16 +0300

    Add files via upload

---
+++ FileNotFoundException.java
@@ -0,0 +1,11 @@
+package Exceptions.HW3;
+
+import java.io.IOException;
+
+public class FileNotFoundException extends IOException {
+
+
+    public FileNotFoundException(String massage) {
+        super(massage);
+    }
+}
